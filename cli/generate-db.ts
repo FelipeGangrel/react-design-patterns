ee62838599ac143c81caab07f46c782a5aa2d2b5
Felipe Gangrel <felipefrog@gmail.com>
2023-07-08 11:12:57 -0300

    Creates a cli program to generate json data

---
+++ cli/generate-db.ts
@@ -0,0 +1,25 @@
+import fs from "fs";
+import { program } from "commander";
+import { type SexType, faker } from "@faker-js/faker";
+
+program.option("-u, --users <number>", "number of users to generate", "20");
+program.parse(process.argv);
+
+const { users } = program.opts<{ users: number }>();
+
+const usersArray = Array.from({ length: users }, () => {
+  const sex: SexType = Math.random() > 0.5 ? "female" : "male";
+  const firstName = faker.person.firstName(sex);
+  const lastName = faker.person.lastName();
+  const email = faker.internet.email({ firstName, lastName }).toLowerCase();
+  const image = `https://xsgames.co/randomusers/avatar.php?g=${sex}`;
+  const id = faker.string.uuid();
+
+  return { id, firstName, lastName, email, image };
+});
+
+const data = {
+  users: usersArray,
+};
+
+fs.writeFileSync("./db.json", JSON.stringify(data, null, 2));
